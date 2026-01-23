fd010e7390d5c812c4dbdb587c134483ee54c44b
Valerio Molinari <there.is.no.spoon.2159@gmail.com>
2026-01-23 09:35:24 +0100

    Setup RubricaAdoNet app

---
+++ RubricaAdoNet/RubricaAdoNet/Models/Contatto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RubricaAdoNet.Models
+{
+    public class Contatto
+    {
+        public required int Id { get; set; }
+        public required string Nome { get; set; }
+        public required string Cognome { get; set; }
+        public string? NumeroCellulare { get; set; }
+        public string? NumeroFisso { get; set; }
+        public string? Email { get; set; }
+        public string? Citta { get; set; }
+        public DateTime? DataDiNascita { get; set; }
+    }
+}
